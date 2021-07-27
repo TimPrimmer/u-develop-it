64142f2ed1fff54b03a51375e70f540d109386c6
TimPrimmer <timprimmer@gmail.com>
2021-07-27 17:48:08 -0500

    Update server.js

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const db = mysql.createConnection(
     // Your MySQL username,
     user: 'root',
     // Your MySQL password
-    password: 'Peppy023',
+    password: '######',
     database: 'election'
   },
   console.log('Connected to the election database.')
